46c864a5e5410c90a1aed68402c9847d7d8c8955
madansalesforce <madan.salesforce17@gmail.com>
2021-09-27 12:19:43 -0400

    intial commit

---
+++ .sfdx/typings/lwc/apex/createGuestRecord.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/createGuestRecord.InsertHotelFields" {
+  export default function InsertHotelFields(param: {GHotelCity: any, GHotelType: any}): Promise<any>;
+}
+declare module "@salesforce/apex/createGuestRecord.InsertGuestFields" {
+  export default function InsertGuestFields(param: {GFName: any, GLName: any, GPhone: any, GEmail: any}): Promise<any>;
+}
